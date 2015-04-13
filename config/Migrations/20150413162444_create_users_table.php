f5887c5766bc29d17475f4c9bfd8a56f7d980aea
Shaun Gordon <shaundev.gordon@gmail.com>
2015-04-13 17:38:19 +0100

    database config and migrations

---
+++ config/Migrations/20150413162444_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users_tables');
+        $table->addColumn('first_name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('last_name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('email', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('telephone', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('gender', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('dob', 'date', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('comments', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
